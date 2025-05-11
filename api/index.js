08b6256a2ef3650200502ca6d88495a2c0fb59a3
Pratishtha <nandwalpratishtha06@gmail.com>
2025-05-12 02:44:28 +0530

    Push changes

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use('/uploads',express.static(__dirname + '/uploads'));
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
     credentials:true,
-    origin:'https://airbnb-adeu.onrender.com',
+    origin:'https://air-bnb--phi.vercel.app',
 }));
 
 const jwtSecret =process.env.JWT_SECRET;
