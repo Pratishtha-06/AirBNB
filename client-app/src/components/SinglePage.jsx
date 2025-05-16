48770d03434ee4f814a80a6fc342abc9e33ec504
Pratishtha <nandwalpratishtha06@gmail.com>
2025-05-16 13:28:52 +0530

    ADD CHANGES

--- client-app/src/components/SinglePage.jsx
+++ client-app/src/components/SinglePage.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Booking from "./BookingWidget";
 import Gallery from "./PlaceGallery";
 import DOMPurify from 'dompurify' ;
+import Heart from '../assets/heart.png';
 
 function SinglePage(){
     const {id} =useParams();
@@ -26,14 +27,21 @@ function SinglePage(){
     return(
         <>
         <div className="container" style={{backgroundColor:'rgba(231,236,240,1)'}}>
-            <h4 className="mx-3 pt-4">{place.title}</h4>
-            <a target="blank" href={`https://maps.google.com/?q=${place.address}`} style={{textDecorationColor:"black",display:'flex'}}>
-               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" style={{width:'18px',height:'18px',color:'black',marginLeft:'8px'}}>
-               <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-               <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-               </svg>
-               <h6  className=" text-black ">{place.address}</h6>
-            </a>
+            <div className="d-flex flex-row justify-content-between align-items-center">
+              <div>
+                <h4 className="mx-3 pt-4">{place.title}</h4>
+                <a target="blank" href={`https://maps.google.com/?q=${place.address}`} style={{textDecorationColor:"black",display:'flex'}}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" style={{width:'18px',height:'18px',color:'black',marginLeft:'8px'}}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                  </svg>
+                  <h6  className=" text-black ">{place.address}</h6>
+                </a>
+              </div>
+            <div>
+                <img src={Heart} style={{width:'15px',height:'15px',marginRight:'10px'}}/>
+            </div>
+           </div>
             
          <Gallery place={place}/>
 
