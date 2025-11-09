866c403988acf169a0fc528c1edca1069db393fc
Pratishtha <nandwalpratishtha06@gmail.com>
2025-11-09 20:46:00 +0530

    Changes

--- api/index.js
+++ api/index.js
@@ -354,6 +354,14 @@ app.get('/api/get-key',async(req,res)=>{
     res.status(200).json({key:Test_Key});
 })
 
+//Payment-Verifiation
+app.get('/api/paymentVerification',async(req,res)=>{
+console.log(req.body);
+res.status(200).json({success:true});
+
+})
+
+
 //Port Connection-----
 app.listen(port,()=>{
     console.log("Server started on Port : 4000");
